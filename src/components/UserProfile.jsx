d548a55aa0d51be2d4bc66484ae04b0bdd42c05a
Badmus Adeshina <badmusadeshinna@gmail.com>
2022-01-19 12:03:05 +0100

    first frontend commit

---
+++ src/components/UserProfile.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UserProfile = () => {
+  return <div>user</div>;
+};
+
+export default UserProfile;
---
+++ src/container/Pins.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Pins = () => {
+  return <div>pin</div>;
+};
+
+export default Pins;
